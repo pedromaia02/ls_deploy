8c8f8698d831e55d2faab432ba44671f40f6663a
pedromaia02 <pedromaia02@gmail.com>
2016-10-10 18:46:27 -0300

    add login 2

--- lsapache/src/posts/views.py
+++ lsapache/src/posts/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib import messages
 from django.db.models import Sum
 import json
+from django.contrib.auth.decorators import login_required
 # from django.views.generic.edit import DeleteView
 # from django.urls import reverse_lazy
 
